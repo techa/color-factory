3716a86db70e5a304e19d277ab77d07f8a295e76
techa <techa27@gmail.com>
2017-02-19 12:34:02 +0900

    fix: delete color-tip.tag

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import riot from 'riot'
 import './components/app.tag'
 import './components/color-card.tag'
-import './components/color-tip.tag'
 import './components/color-lists.tag'
 import './components/context-menu.tag'
 import './components/color-wheel.tag'
