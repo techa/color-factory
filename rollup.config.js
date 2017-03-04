3bc605dadfa58a4b41477f6513dcd3805019994a
techa <techa27@gmail.com>
2017-03-05 05:14:19 +0900

    update rollup.config

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,61 @@
-import riot from 'rollup-plugin-riot'
 import commonjs    from 'rollup-plugin-commonjs'
 import nodeResolve from 'rollup-plugin-node-resolve'
 
 import json from 'rollup-plugin-json'
 import replace from 'rollup-plugin-replace'
 
+import svelte from 'rollup-plugin-svelte'
+import riot from 'rollup-plugin-riot'
+
+import buble from 'rollup-plugin-buble'
+import uglify from 'rollup-plugin-uglify'
+import savelicense from 'uglify-save-license'
+
+const env = (process.env.NODE_ENV || 'development').trim()
+const suffix = (env === 'production') ? '.min' : (env === 'svelte') ? '.svelte' : ''
+
 export default {
-  entry: 'src/index.js',
-  dest: `dist/bundle.js`,
+  entry: env === 'svelte' ? 'src/svelte.js' : 'src/index.js',
+  dest: `dist/bundle${suffix}.js`,
   // https://github.com/rollup/rollup/wiki/JavaScript-API#format
   format: 'iife', // es(default),cjs,iife
   sourceMap: true,
   // http://qiita.com/cognitom/items/e3ac0da00241f427dad6#appendix
   plugins: [
     commonjs(), // CommonJSモジュールをES6に変換
-    json({
-      preferConst: true
-    }),
-    riot({
-      include: [
-        './src/components/**.tag',
-      ],
-    }),
+    json({preferConst: true}),
+    (env === 'svelte'
+      ? svelte({
+        include: [
+          './src/svelte/**.html',
+        ],
+      })
+      : riot({
+        include: [
+          './src/components/**.tag',
+        ],
+      })
+    ),
     nodeResolve({
       jsnext: true, // if provided in ES6
       main: true, // if provided in CommonJS
       browser: true, // if provided for browsers
     }), // npmモジュールを`node_modules`から読み込む
     replace({
-      'process.env.NODE_ENV': JSON.stringify('production')
+      'process.env.NODE_ENV': JSON.stringify(env)
     }),
+    (env === 'production' && buble()),
+    (env === 'production' && uglify({
+      compress: {
+        dead_code: true,
+        global_defs: {
+          DEBUG: false
+        },
+        drop_console: true
+      },
+      output: {
+        comments: savelicense
+      }
+    })),
   ],
 }
