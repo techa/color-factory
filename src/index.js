f6c0c594d0a526806c02d281bd5dcc111102390e
techa <techa27@gmail.com>
2018-05-04 06:43:53 +0900

    Fix: Load defaultpalette

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import App from './svelte/app.html'
 import store from './store/store.js'
 import defaultpalette from './constants/newwebcolor'
 import './Color-ex'
-store.save(defaultpalette.paletteName, defaultpalette)
+
+// Load defaultpalette
+if (!window.localStorage.getItem(defaultpalette.paletteName)) {
+  store.save(defaultpalette.paletteName, defaultpalette)
+}
 
 new App({
   target: document.querySelector('#root'),
